a6a46d02ba14f2b754225c5feaf1de7399b67e49
itpreneur@yahoo.com <itpreneur@yahoo.com>
2019-02-08 14:40:22 -0500

    initial configuration

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,4 @@
+//const chalk = require("chalk"); //es2015 syntax prior to using babel preset latest
+import chalk from "chalk"; //es6
+
+console.log(chalk.green("Starting app in dev mode...")); // eslint-disable-line no-console
